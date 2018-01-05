448b7fa1d7d050cfd1b0b187fbce4fe3565dfdd6
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2018-01-05 13:16:55 +0100

    Properly working vertcoin faucet

--- index.js
+++ index.js
@@ -2,12 +2,24 @@
 
 var http = require('http')
 var path = require('path')
-var Blockchain = require('cb-insight')
 var chalk = require('chalk')
 var express = require('express')
 var fs = require('fs')
+var request = require('request')
 var bitcoin = require('bitcoinjs-lib')
+var QRCode = require('qrcode');
+var vertcoinNetwork = {
+  messagePrefix: 'Vertcoin Signed Message:\n',
+  bip32: {
+    public: 0x043587cf,
+    private: 0x04358394
+  },
+  pubKeyHash: 0x4A,
+  scriptHash: 0xC4,
+  wif: 0xEF
+};
 
+var usedAddresses = [];
 var PORT = process.env.FAUCET_PORT || process.env.PORT || 14004
 
 var privkey = process.env.PRIVKEY
@@ -17,75 +29,108 @@ if (privkey == undefined) {
 
   // initialize wallet
   if (!fs.existsSync(WALLET_FILE)) {
-    privkey = bitcoin.ECPair.makeRandom({network: bitcoin.networks.testnet, compressed: false}).toWIF()
+    privkey = bitcoin.ECPair.makeRandom({network: vertcoinNetwork, compressed: false}).toWIF()
     fs.writeFileSync(WALLET_FILE, privkey, 'utf-8')
   } else {
     privkey = fs.readFileSync(WALLET_FILE, 'utf-8')
   }
 }
 
-var keypair = bitcoin.ECPair.fromWIF(privkey)
+
+
+var keypair = bitcoin.ECPair.fromWIF(privkey, vertcoinNetwork)
 var address = keypair.getAddress().toString()
 
-var blockchain = new Blockchain('https://test-insight.bitpay.com')
+QRCode.toFile(path.join(__dirname,"address-qr.png"), "vertcoin:" + address, {}, function (err) {
+  if (err) throw err
+  console.log('done')
+});
+
+var template = "<h1>Starting up please retry later</h1>";
+fs.readFile(path.join(__dirname,"index.html"), (err, data) => {
+  template = data.toString('ascii');
+  template = template.replace("%address%", address);
+});
+
+var sentTemplate = "<h1>Starting up please retry later</h1>";
+fs.readFile(path.join(__dirname,"sent.html"), (err, data) => {
+  sentTemplate = data.toString('ascii');
+});
 
 var app = express()
 app.get('/', function (req, res) {
-  var pkg = require('./package')
-  res.set('Content-Type', 'text/plain')
-  res.end('bitcoin-faucet version: ' + pkg.version + '\n\nPlease send funds back to: ' + address)
-})
+  request.get('https://tvtc.blkidx.org/addressBalance/' + address + '?unconfirmed=1', {json:false}, (err, result, body) => {
+    if(err) return res.sendStatus(500);
+    res.header("Content-Type","text/html");
+    res.send(template.replace("%balance%", parseInt(body)/100000000));
+  });
+});
+
+app.get('/qr.png', function(req,res) {
+  res.header("Content-Type", "image/png");
+  res.sendFile(path.join(__dirname,"address-qr.png"));
+});
 
 // only bitcoin testnet supported for now
 app.get('/withdrawal', function (req, res) {
   if (!req.query.address) {
-    res.status(422).send({ status: 'error', data: { message: 'You forgot to set the "address" parameter.' } })
+    return res.status(422).send({ status: 'error', data: { message: 'You forgot to set the "address" parameter.' } });
+  }
+  try {
+    var addy = bitcoin.address.fromBase58Check(req.query.address);
+    if(addy.version != vertcoinNetwork.pubKeyHash) {
+      return res.status(422).send({ status: 'error', data: { message: 'Invalid address version byte' } });
+    }
+  } catch(e) {
+    console.log(e);
+    return res.status(422).send({ status: 'error', data: { message: 'Invalid address' } });
+  }
+  
+  if (usedAddresses.find((s) => { return (s == req.query.address); }) != null) {
+    return res.status(422).send({ status: 'error', data: { message: 'You already received some testnet coins on this address.' } });
   }
 
+  usedAddresses.push(req.query.address);
+
   // satoshis
-  var amount = parseInt(req.query.amount, 10) || 10000
+  var amount = parseInt(req.query.amount, 10) || 500000000
+  if(amount > 500000000) amount = 500000000
 
   spend(keypair, req.query.address, amount, function (err, txId) {
     if (err) return res.status(500).send({status: 'error', data: {message: err.message}})
-    res.send({status: 'success', data: {txId: txId}})
+    res.header("Content-Type","text/html");
+    res.send(sentTemplate.replace("%txid%", txId));
   })
-})
+});
 
 function spend(keypair, toAddress, amount, callback) {
-  blockchain.addresses.unspents(address, function (err, utxos) {
+  request.get('https://tvtc.blkidx.org/addressTxos/' + address + '?unconfirmed=1', {json:true}, (err, result, body) => {
     if (err) return callback(err)
-
-    var balance = utxos.reduce(function (amount, unspent) {
-      return unspent.value + amount
-    }, 0)
-
-    if (amount > balance) {
+    
+    var tx = new bitcoin.TransactionBuilder(vertcoinNetwork);
+    var total = 0;
+    body.forEach((txo) => {
+        if(txo.spender == null) {
+            if(total <= (amount + 100000)) {
+                total += txo.value;
+                tx.addInput(txo.txhash,txo.vout);
+            }
+        }
+    });
+    if (amount + 100000 > total) {
       return callback(new Error('Address doesn\'t contain enough money to send.'))
     }
-
-    var tx = new bitcoin.TransactionBuilder()
-    tx.addOutput(toAddress, amount)
-
-    var change = balance - amount
-    if (change > 0) {
-      tx.addOutput(address, change)
-    }
-
-    utxos.forEach(function (unspent) {
-      tx.addInput(unspent.txId, unspent.vout)
-    })
-
-    utxos.forEach(function (unspent, i) {
-      tx.sign(i, keypair)
-    })
-
-    var txHex = tx.build().toHex()
-    blockchain.transactions.propagate(txHex, function (err, result) {
+    tx.addOutput(address, total-amount-100000);
+    tx.addOutput(toAddress, amount);
+    tx.inputs.forEach((input, i) => {
+      tx.sign(i, keypair);
+    });
+    var txHex = tx.build().toHex();
+    request.post('https://tvtc.blkidx.org/sendRawTransaction', { body : txHex }, (err, result, body) => {
       if (err) return callback(err)
-
-      callback(null, result.txId)
-    })
-  })
+      callback(null, body);
+    });
+  });
 }
 
 var server = http.createServer(app)
